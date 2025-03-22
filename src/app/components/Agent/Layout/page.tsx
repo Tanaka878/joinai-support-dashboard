a852b9423e25bc9489bfb67391a3ef923cb2dc55
Tanaka878 <musungaretanaka@gmail.com>
2025-03-22 11:31:37 +0200

    adding the layout component to the agent Dashboard

--- src/app/components/Agent/Layout/page.tsx
+++ src/app/components/Agent/Layout/page.tsx
@@ -1,46 +1,20 @@
 'use client';
 import React, { useState } from 'react';
-import NavBar from '../../Agent/NavBar/page';
-import SideNav from '../AdminNavigation/page';
-import { useRouter } from 'next/navigation';
-import AgentDataComponent from '../AgentsData/page';
-import TicketList from '../TicketData/page';
-import Reports from '../Reports/page';
+import Tickets from '../Tickets/page';
+import SideNav from '../SideNav/page';
+import NavBar from '../NavBar/page';
 
-interface NavBar {
-  onModalChange: () => void;
-  showNotifications: () => void;
-}
-
-const Layout: React.FC<NavBar> = ({}) => {
-  const [currentView, setCurrentView] = useState('Dashboard');
+const Layout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true); // Track sidebar state
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isNotificationTabOpen, setIsNotificationTabOpen] = useState(false);
-  const router = useRouter();
   
   const toggleModal = () => setIsModalOpen(!isModalOpen);
   const toggleNotifications = () => setIsNotificationTabOpen(!isNotificationTabOpen);
   
-  function Logout() {
-    router.push('/components/LoginPage');
-  }
   
-  // Render the current view based on the state
-  const renderContent = () => {
-    switch (currentView) {
-      case 'Dashboard':
-        return <AgentDataComponent />;
-      case 'Tickets':
-        return <TicketList />;
-      case 'Agents':
-        return <Reports />; // Replace with your Agents component
-      case 'Statistics':
-        return <AgentDataComponent />; // Replace with your Statistics component
-      default:
-        return <TicketList />;
-    }
-  };
+
+ 
   
   return (
     <div className="h-screen flex flex-col overflow-hidden">
@@ -58,13 +32,12 @@ const Layout: React.FC<NavBar> = ({}) => {
         <div className={`flex-none ${isSidebarOpen ? 'w-64' : 'w-16'} transition-width duration-300 ease-in-out`}>
           <SideNav
             isSidebarOpen={isSidebarOpen}
-            onSelectPage={(view) => setCurrentView(view)}
           />
         </div>
         
         {/* Main Dashboard Content - takes remaining space */}
         <div className="flex-1 bg-gray-100 p-6 overflow-auto mt-12 text-black">
-          {renderContent()}
+            <Tickets/>
         </div>
       </div>
       
