4a9556815f4b82fe094ad48d4f8423fef9520a83
Tanaka878 <musungaretanaka@gmail.com>
2025-04-03 12:00:51 +0200

    commit

--- src/app/config/api/api.ts
+++ src/app/config/api/api.ts
@@ -1,3 +1,8 @@
-const BASE_URL = "http://localhost:8082"; 
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const BASE_URL =  "http://localhost:8082"; 
 
 export default BASE_URL;
