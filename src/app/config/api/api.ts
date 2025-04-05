a2a003769be9203835f6e24ca1823018fe56f4d1
Tanaka878 <musungaretanaka@gmail.com>
2025-04-05 10:06:34 +0200

    editing json file and the config files

--- src/app/config/api/api.ts
+++ src/app/config/api/api.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
-const BASE_URL =  "http://localhost:8082"; 
+const BASE_URL =  "http://localhost:8082" ; 
 
 export default BASE_URL;
