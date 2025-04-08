c4ca9718cdaba6fc2bf5e04290cf3b1a1149695d
Tanaka878 <musungaretanaka@gmail.com>
2025-04-08 16:31:33 +0200

    changing the system to fetch from .env file

--- src/app/config/api/api.ts
+++ src/app/config/api/api.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const BASE_URL =  "http://localhost:8082" ; 
+//const BASE_URL =  "http://localhost:8082"; 
+const BASE_URL = process.env.NEXT_PUBLIC_PROD;
 
 export default BASE_URL;
