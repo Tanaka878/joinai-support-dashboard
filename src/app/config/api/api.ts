c7dec0b40c47c41ec95530bd40d78d35db07ff88
Tanaka878 <musungaretanaka@gmail.com>
2025-07-19 12:23:26 +0200

    changing the base url to the production server

--- src/app/config/api/api.ts
+++ src/app/config/api/api.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 //const BASE_URL =  "http://localhost:8082"; 
-const BASE_URL = "http://localhost:8081"
+const BASE_URL = "https://support.thetalisman.co.zw"
 
 export default BASE_URL;
