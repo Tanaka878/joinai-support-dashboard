9c89802f6fc4002e7a26cb426f17e2e945a9a7e7
Tanaka878 <musungaretanaka@gmail.com>
2025-08-19 22:39:57 +0200

    commit

--- src/app/config/api/api.ts
+++ src/app/config/api/api.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const BASE_URL =  "http://localhost:8082"; 
-//const BASE_URL = "https://support.thetalisman.co.zw"
+//const BASE_URL =  "http://localhost:8082"; 
+const BASE_URL = "https://support.thetalisman.co.zw"
 
 export default BASE_URL;
